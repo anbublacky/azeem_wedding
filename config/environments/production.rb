403659e5b492773ced458876209ee9aa960f39d2
Anbazhagan <anbublacky@gmail.com>
2019-03-10 21:34:30 +0530

    Production initializer changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
